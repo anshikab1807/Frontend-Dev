48fc7726f48164a1f62e2a32df932ca9caff37c5
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-11-15 16:38:45 +0530

    Add files via upload

---
+++ JS Operators/UtilityDashboard.js
@@ -0,0 +1,7 @@
+let x = 16.75;
+let roundedValue = Math.round(x);
+let squareRoot = Math.sqrt(x);
+let powerValue = Math.pow(x, 3);
+let randomBetween10and50 = Math.floor(Math.random() * 41) + 10;
+console.log(`Math Utility Dashboard:\n
+Rounded Value: ${roundedValue}\nSquare Root: ${squareRoot}\nX to the power 3: ${powerValue}\nRandom Number (10-50): ${randomBetween10and50}`);
