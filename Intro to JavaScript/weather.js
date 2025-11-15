d54fa04499e914a899cd028ccb9caac35ab5419c
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-11-15 16:26:56 +0530

    Add files via upload

---
+++ Intro to JavaScript/weather.js
@@ -0,0 +1,12 @@
+const temperature = 28;
+const isRaining = false;
+const windSpeed = 10;
+if (isRaining) {
+  console.log("Stay indoors with hot coffee.");
+} else if (temperature > 35) {
+  console.log("Go swimming.");
+} else if (temperature < 15 && windSpeed > 20) {
+  console.log("Too cold and windy");
+} else {
+  console.log("Perfect day for a walk");
+}
