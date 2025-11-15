7d5cafc86da8db7f1c07ad30ab3577e88075bbea
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-11-15 16:28:48 +0530

    Add files via upload

---
+++ Intro to JavaScript/guessGame.js
@@ -0,0 +1,14 @@
+const secret = Math.floor(Math.random() * 50) + 1;
+const userGuess = 45;
+if (userGuess === secret) {
+  console.log("Correct guess!");
+} else if (Math.abs(userGuess - secret) <= 3) {
+  console.log("Very close!");
+} else {
+  if (userGuess > secret) {
+    console.log("Too high");
+  } else {
+    console.log("Too low");
+  }
+}
+console.log("Secret Number was:", secret);
