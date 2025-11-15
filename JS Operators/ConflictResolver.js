0e26960c72b6fda8eefaf040a36fef38ae4a7bfb
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-11-15 16:35:22 +0530

    Add files via upload

---
+++ JS Operators/ConflictResolver.js
@@ -0,0 +1,12 @@
+let bonus = 5000;
+function calculateSalary() {
+let salary = 40000;
+let isPermanent = true;
+if (isPermanent) {
+    salary += bonus;
+  }
+  console.log("Total Salary:", salary);
+  console.log("Is Permanent:", isPermanent);
+  console.log("Global Bonus (unchanged):", bonus);
+}
+calculateSalary();
