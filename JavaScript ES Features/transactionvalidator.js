3f71a2fccb88a62337a8e4073482a5927be742eb
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-11-19 22:56:30 +0530

    Add files via upload

--- JavaScript ES Features/transactionvalidator.js
+++ JavaScript ES Features/transactionvalidator.js
@@ -1,28 +1,21 @@
-"use strict";
-
-// Custom Error Classes
 class NullEntryError extends Error {
     constructor(message) {
         super(message);
         this.name = "NullEntryError";
     }
 }
-
 class MissingFieldError extends Error {
     constructor(message) {
         super(message);
         this.name = "MissingFieldError";
     }
 }
-
 class NegativeAmountError extends Error {
     constructor(message) {
         super(message);
         this.name = "NegativeAmountError";
     }
 }
-
-// Transactions list
 const transactions = [
     { id: 1, amount: 2000 },
     { id: 2, amount: -500 },
@@ -35,7 +28,6 @@ const invalid = [];
 
 transactions.forEach((txn, index) => {
     try {
-        // Debug breakpoint to inspect variables
         debugger;
 
         if (txn === null) {
@@ -49,8 +41,6 @@ transactions.forEach((txn, index) => {
         if (txn.amount < 0) {
             throw new NegativeAmountError(`Negative amount detected for ID ${txn.id}`);
         }
-
-        // If valid
         valid.push(txn);
         console.log(` Valid Transaction:`, txn);
 
@@ -63,8 +53,6 @@ transactions.forEach((txn, index) => {
         console.error(` Error [${error.name}] → ${error.message}`);
     }
 });
-
-// Final Summary Report
 console.log("\n ");
 console.log("        FINAL REPORT");
 console.log(" \n");
@@ -75,3 +63,4 @@ console.log(" Invalid Transactions:", invalid);
 console.log("\nSummary:");
 console.log(`Total Valid:   ${valid.length}`);
 console.log(`Total Invalid: ${invalid.length}`);
+
